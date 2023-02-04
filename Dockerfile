74b81fe6474599d8b2ac48aaabef9ead845fa993
shyam <shyamayadav154@gmail.com>
2023-02-04 20:10:58 +0530

    pupetter-core removed

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM node:16-alpine
 WORKDIR /app
 COPY package.json .
-RUN npm install --only=production
+RUN npm install --omit=dev
 COPY . .
 EXPOSE 4000
 CMD ["npm", "start"]
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "server.bundle.js",
+  },
+  target: "node",
+};
