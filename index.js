2a3faf4de6f46ed4461f39960deecdbb8322dca5
shyam <shyamayadav154@gmail.com>
2023-02-04 22:10:04 +0530

    one cmd commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get("/og-image", async (req, res) => {
 
   try {
     const browser = await puppeteer.launch({
-      // executablePath: "/usr/bin/google-chrome",
+      executablePath: "/usr/bin/google-chrome",
       args: ["--no-sandbox"],
       headless: true,
     });
