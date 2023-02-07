cc4be653a3894841b10e508efe21146b393ff9c3
shyam <shyamayadav154@gmail.com>
2023-02-07 10:53:37 +0530

    one cmd commit

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const puppeteer = require("puppeteer");
 
+const pup = require("./puppeteer");
+
 const app = express();
 
-app.get("/og-image", async (req, res) => {
+app.get("/og-image-2", async (req, res) => {
   const link = req.query.link;
   if (!link) {
     return res.status(400).send({ error: "Link parameter is required" });
   }
-  
-
-  
 
   try {
     const browser = await puppeteer.launch({
@@ -21,28 +20,25 @@ app.get("/og-image", async (req, res) => {
 
     const page = await browser.newPage();
     await page.setViewport({ width: 1366, height: 768 });
-    await page.goto(link, { waitUntil: "networkidle2", });
-    //if 404 retry
+    await page.goto(link, { waitUntil: "networkidle2" });
+    // retry if 404
     if (page.url().includes("404")) {
-      await page.goto(link, { waitUntil: "networkidle2", });
+      await page.goto(link, { waitUntil: "networkidle2" });
     }
     const screenshot = await page.screenshot({
       type: "jpeg",
       clip: { x: 240, y: 20, width: 900, height: 600 },
     });
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "image/jpeg");
-      res.setHeader(
-        "Cache-Control",
-        `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`
-      );
+    await browser.close();
 
-    res.end(screenshot);
+    res.set("Content-Type", "image/jpeg");
+    res.send(screenshot);
   } catch (error) {
     res.status(500).send({ error: error.message });
   }
-  await browser.close();
 });
+
+app.get("/og-image",pup.generateImage )
 
 app.listen(4000, () => {
   console.log("OG image API running on port 4000");
---
+++ puppeteer.js
@@ -0,0 +1,51 @@
+const puppeteer = require("puppeteer");
+
+exports.generateImage = async function (req, res) {
+  const { link } = await req.query;
+
+  if(!link) {
+    return res.status(400).send({ error: "Link parameter is required" });
+    }
+
+    // check if link is valid
+    const regex = new RegExp(
+        "^(https?:\\/\\/)?" + // protocol
+        "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+        "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+        "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+        "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+        "(\\#[-a-z\\d_]*)?$",
+        "i"
+    ); // fragment locator
+    if (!regex.test(link)) {
+        return res.status(400).send({ error: "Link parameter is not valid" });
+    }
+
+
+   const browser = await puppeteer.launch({
+    executablePath: "/usr/bin/chromium-browser",
+    args: ["--no-sandbox"],
+    headless: true,
+
+   });
+
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1366, height: 768 });
+  await page.goto(link, { waitUntil: "networkidle2" });
+  // retry if 404
+  if (page.url().includes("404")) {
+    await page.goto(link, { waitUntil: "networkidle2" });
+  }
+  const image = await page.screenshot({
+    type: "jpeg",
+    clip: { x: 240, y: 20, width: 900, height: 600 },
+  });
+
+  res.statusCode = 200;
+  res.setHeader("Content-Type", `image/jpeg`);
+  res.setHeader(
+    "Cache-Control",
+    `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`
+  );
+  res.end(image);
+};
